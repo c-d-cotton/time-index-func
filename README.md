057876f734bc9a349bdb9f41de6f552038d8c3b6
Ryan C Wallace <Ryan.Wallace@bos.frb.org>
2024-03-26 18:19:14 -0400

    build: add metadata required to make time_index_func an installable package

---
+++ README.md
@@ -0,0 +1,1 @@
+# `time_index_func`
---
+++ time_index_func/__init__.py
@@ -0,0 +1,5 @@
+# Import all names from the time_index_func module directly into the
+# top-level package namespace. This pattern should typically not be used
+# to avoid cluttering the namespace, but this case of a package with a
+# single module is reason for an exception for the sake of convenience.
+from .time_index_func import *
